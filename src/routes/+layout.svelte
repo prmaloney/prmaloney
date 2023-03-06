83183946144a86ab0747f492110e51290227d91e
Patrick Maloney <patrick@foobar.agency>
2023-03-06 14:07:28 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	const links = [
+		{
+			href: '/home',
+			title: 'Home'
+		},
+		{
+			href: '/blog',
+			title: 'Blog'
+		},
+		{
+			href: '/projects',
+			title: 'Projects'
+		},
+		{
+			href: '/about',
+			title: 'About'
+		}
+	];
+</script>
+
+<div>
+	{#each links as { href, title }}
+		<a {href} class:active={$page.url.pathname === href}>{title}</a>
+	{/each}
+</div>
+
+<slot />
+
+<style>
+	a.active {
+		background: gray;
+	}
+</style>
