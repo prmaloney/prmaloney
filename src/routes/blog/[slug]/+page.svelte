24dfb9483e1ece497148f13558393dc654dd5734
Patrick Maloney <patrick@foobar.agency>
2023-03-07 16:03:59 +0100

    add blog post page and style homepage

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -5,5 +5,16 @@
 </script>
 
 <h1>{title}</h1>
-<img src={coverImage.url} alt="{title} cover image" />
+<div class="img-wrapper">
+	<img src={coverImage.url} alt="{title} cover image" />
+</div>
 <p>{@html content}</p>
+
+<style>
+	.img-wrapper {
+		max-width: 50rem;
+	}
+	.img-wrapper > img {
+		max-width: 100%;
+	}
+</style>
