1cc91cd9babe9237d70f0a91d685099ad235d411
Patrick Maloney <patrick@foobar.agency>
2023-05-09 20:46:48 +0200

    update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,10 @@
 <h1>About this site</h1>
 
-<p>Here's some info about this site.</p>
+<p>Thanks for visiting my portfolio!</p>
+<p>Take a look at the project description for this site <a href="/projects/portfolio">here</a>.</p>
+
+<style>
+	a {
+		color: #9ccfd8;
+	}
+</style>
