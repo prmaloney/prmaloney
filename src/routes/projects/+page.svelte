9ad0f4352cda87151db5395eb38612baf271f34b
Patrick Maloney <patrick@foobar.agency>
2023-05-10 17:55:59 +0200

    work on responsiveness

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,8 +6,12 @@
 <h1>Projects</h1>
 
 {#each data.projects as project}
-	<a class="flex flex-row after:content-none" href="/projects/{project.slug}">
-		<img class="w-64 h-64 object-contain" src={project.image[0]?.url} alt={project.name} />
+	<a class="flex flex-row after:content-none gap-4" href="/projects/{project.slug}">
+		<img
+			class="w-32 h-32 md:w-64 md:h-64 object-contain"
+			src={project.image[0]?.url}
+			alt={project.name}
+		/>
 		<div class="flex flex-col">
 			<h2>{project.name}</h2>
 			<div class="flex flex-row gap-4">
@@ -18,27 +22,3 @@
 		</div>
 	</a>
 {/each}
-
-<!-- <style lang="scss"> -->
-<!-- 	a { -->
-<!-- 		display: flex; -->
-<!-- 		flex-direction: row; -->
-<!-- 		gap: 2rem; -->
-<!-- 		margin-bottom: 2rem; -->
-<!-- 		img { -->
-<!-- 			width: 200px; -->
-<!-- 			height: 200px; -->
-<!-- 			object-fit: contain; -->
-<!-- 		} -->
-<!-- 		&:hover { -->
-<!-- 			text-decoration-color: #ebbcba; -->
-<!-- 			text-underline-offset: 0.4em; -->
-<!-- 		} -->
-<!-- 		span { -->
-<!-- 			margin-right: 1rem; -->
-<!-- 			color: #6e6a86; -->
-<!-- 			text-decoration: none; -->
-<!-- 			display: inline-block; -->
-<!-- 		} -->
-<!-- 	} -->
-<!-- </style> -->
