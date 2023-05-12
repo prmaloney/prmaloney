0b9d7d549c01db68eebdb8aabe557a94194c7126
Patrick Maloney <patrick@foobar.agency>
2023-05-12 21:14:38 +0200

    more responsivity adjustments for project details page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
 </script>
 
 <div>
-	<div class="flex flex-col items-center">
-		<h1>Patrick Maloney</h1>
+	<div class="flex flex-col items-center justify-center md:justify-start">
+		<h1 class="text-center md:text-left">Patrick Maloney</h1>
 		<img
 			class="h-40 w-40 md:h-64 md:w-64 object-cover rounded-full mb-4"
 			src="photo_patrick01.jpg"
--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -7,20 +7,23 @@
 	const content = marked.parse(data.description);
 </script>
 
-<div class="project-header">
+<div class="flex flex-col items-center gap-4 mb-8 md:items-start">
+	<h1>{data.name}</h1>
 	<img src={data.image[0].url} alt={data.name} />
-	<div class="project-header-text">
-		<h1>{data.name}</h1>
-		<div class="tags">
-			{#each data.tags as tag}
-				<span class="tag">{tag}</span>
-			{/each}
-		</div>
-		<a class="text-white" href={data.sourceCode} target="_blank" rel="noreferrer">
-			<Icon name="github" />
-			View Source
-		</a>
+	<div class="flex flex-row gap-2 text-muted">
+		{#each data.tags as tag}
+			<span class="tag">{tag}</span>
+		{/each}
 	</div>
+	<a
+		class="flex items-center text-white max-w-fit pr-4 pl-2 bg-love"
+		href={data.sourceCode}
+		target="_blank"
+		rel="noreferrer"
+	>
+		<Icon name="github" />
+		View Source
+	</a>
 </div>
 {@html content}
 
