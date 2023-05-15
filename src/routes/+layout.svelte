6547cc4bc985cbf0d58765408610c5a4618f1b84
Patrick Maloney <patrick@foobar.agency>
2023-05-15 10:37:46 -0600

    remove onMount animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,17 +5,10 @@
 	import { cubicInOut } from 'svelte/easing';
 	import type { LayoutData } from './$types';
 	import Burger from '../components/Icons/Burger.svelte';
-	import { onMount } from 'svelte';
 
 	const transitionSpeed = 300;
 
 	export let data: LayoutData;
-
-	let animating = false;
-
-	onMount(() => {
-		animating = true;
-	});
 
 	const links = [
 		{
@@ -69,48 +62,46 @@
 		</div>
 	</header>
 
-	{#if animating}
-		<main class="flex-1 px-12 sm:px-20 md:px-40 lg:px-60 xl:px-80 my-8">
-			{#key data.url}
-				<div
-					in:fly={{ duration: transitionSpeed, y: -50, easing: cubicInOut, delay: transitionSpeed }}
-					out:fly={{ duration: transitionSpeed, y: -50 }}
-				>
-					<slot />
-				</div>
-			{/key}
-		</main>
-		<footer class="w-full bg-surface flex flex-col items-center px-8 py-4">
-			<div class="flex gap-4 items-center mb-1">
-				<h3>Let's get in contact:</h3>
-				<a
-					class="after:content-none bg-love hover:scale-110 transition ease-in-out duration-300"
-					href="https://github.com/prmaloney"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<Icon name="github" />
-				</a>
-				<a
-					class="after:content-none bg-pine hover:scale-110 transition
-          ease-in-out duration-300"
-					href="https://linkedin.com/in/prmaloney"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<Icon name="linkedin" />
-				</a>
-				<a
-					class="after:content-none bg-iris hover:scale-110 transition
-          ease-in-out duration-300"
-					href="mailto:pmaloney16@gmail.com"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<Icon name="mail" />
-				</a>
+	<main class="flex-1 px-12 sm:px-20 md:px-40 lg:px-60 xl:px-80 my-8">
+		{#key data.url}
+			<div
+				in:fly={{ duration: transitionSpeed, y: -50, easing: cubicInOut, delay: transitionSpeed }}
+				out:fly={{ duration: transitionSpeed, y: -50 }}
+			>
+				<slot />
 			</div>
-			<span class="text-sm text-muted">© 2023 Patrick Maloney</span>
-		</footer>
-	{/if}
+		{/key}
+	</main>
+	<footer class="w-full bg-surface flex flex-col items-center px-8 py-4">
+		<div class="flex gap-4 items-center mb-1">
+			<h3>Let's get in contact:</h3>
+			<a
+				class="after:content-none bg-love hover:scale-110 transition ease-in-out duration-300"
+				href="https://github.com/prmaloney"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Icon name="github" />
+			</a>
+			<a
+				class="after:content-none bg-pine hover:scale-110 transition
+          ease-in-out duration-300"
+				href="https://linkedin.com/in/prmaloney"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Icon name="linkedin" />
+			</a>
+			<a
+				class="after:content-none bg-iris hover:scale-110 transition
+          ease-in-out duration-300"
+				href="mailto:pmaloney16@gmail.com"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Icon name="mail" />
+			</a>
+		</div>
+		<span class="text-sm text-muted">© 2023 Patrick Maloney</span>
+	</footer>
 </div>
