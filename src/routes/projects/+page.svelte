8a561ee1b196f9c3cc810a8e407271d70c19606c
Patrick Maloney <patrick@foobar.agency>
2023-05-15 12:00:16 -0600

    grid display for projects on large displays

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,32 +7,34 @@
 
 <h1>Projects</h1>
 
-{#each data.projects as project}
-	<a
-		on:mouseover={() => (hover = project.name)}
-		on:focus={() => (hover = project.name)}
-		on:mouseout={() => (hover = null)}
-		on:blur={() => (hover = null)}
-		class="flex flex-row after:content-none gap-4 mb-8"
-		href="/projects/{project.slug}"
-	>
-		<img
-			class="w-32 h-32 md:w-64 md:h-64 object-contain"
-			src={project.image[0]?.url}
-			alt={project.name}
-		/>
-		<div class="flex flex-col">
-			<h2 class="after:bg-rose relative max-w-fit {hover === project.name ? 'hover' : ''}">
-				{project.name}
-			</h2>
-			<div class="flex flex-row gap-4 flex-wrap">
-				{#each project.tags as tag}
-					<span class="text-muted">{tag}</span>
-				{/each}
+<div class="grid gap-16 grid-cols-1 2xl:grid-cols-2">
+	{#each data.projects as project}
+		<a
+			on:mouseover={() => (hover = project.name)}
+			on:focus={() => (hover = project.name)}
+			on:mouseout={() => (hover = null)}
+			on:blur={() => (hover = null)}
+			class="flex flex-row after:content-none gap-4"
+			href="/projects/{project.slug}"
+		>
+			<img
+				class="w-32 h-32 md:w-52 md:h-52 object-contain"
+				src={project.image[0]?.url}
+				alt={project.name}
+			/>
+			<div class="flex flex-col">
+				<h2 class="after:bg-rose relative max-w-fit {hover === project.name ? 'hover' : ''}">
+					{project.name}
+				</h2>
+				<div class="flex flex-row gap-4 flex-wrap">
+					{#each project.tags as tag}
+						<span class="text-muted">{tag}</span>
+					{/each}
+				</div>
 			</div>
-		</div>
-	</a>
-{/each}
+		</a>
+	{/each}
+</div>
 
 <style>
 	h2::after {
