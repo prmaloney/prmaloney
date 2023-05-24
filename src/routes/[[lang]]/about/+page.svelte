90258f778d62401ec10a25664e000e4ef3ee1978
Patrick Maloney <patrick@foobar.agency>
2023-05-24 09:24:04 +0200

    add lang param

--- src/routes/[[lang]]/about/+page.svelte
+++ src/routes/[[lang]]/about/+page.svelte
@@ -1,4 +1,8 @@
-<h1>About this site</h1>
+<script>
+	import { _ } from 'svelte-i18n';
+</script>
+
+<h1>{$_('about.title')}</h1>
 
 <p>Thanks for visiting my portfolio!</p>
 <p>Take a look at the project description for this site <a href="/projects/portfolio">here</a>.</p>
